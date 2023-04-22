419342f9b90d04d47d252fa1a896ad03c33d882c
rarboless <115485808+rarboless@users.noreply.github.com>
2023-04-22 11:10:06 +0200

    Ordenat i noms correctes

---
+++ Projecte-I (Git)/Assets/Roger_Tests/Scripts/Ninja.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Ninja : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    private Vector3 movement;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementProcess();
+    }
+
+    void MovementProcess() {
+        movement = Vector3.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement != Vector3.zero) {
+            rigidBody.MovePosition(transform.position + movement * velocity);
+            //Amb vSync Activat
+            //ninjaRigidBody.MovePosition(transform.position + movement * velocity * Time.deltaTime);
+
+            animator.SetFloat("moveX", movement.x);
+            animator.SetFloat("moveY", movement.y);
+            animator.SetBool("isWalking", true);
+        }
+        else {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
