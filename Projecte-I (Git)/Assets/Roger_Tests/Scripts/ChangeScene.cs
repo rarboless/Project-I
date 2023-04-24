b30e6b55bb02233ac27ce2d9f2a3f3d8f01d79f0
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2023-04-24 17:49:39 +0200

    SceneFader

--- Projecte-I (Git)/Assets/Roger_Tests/Scripts/ChangeScene.cs
+++ Projecte-I (Git)/Assets/Roger_Tests/Scripts/ChangeScene.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
-{
+public class ChangeScene : MonoBehaviour {
     // Start is called before the first frame update
     public int sceneBuildIndex;
+
+    [SerializeField] private RawImage img;
+    [SerializeField] private AnimationCurve fadeCurve;
+
+    private void Start() {
+        StartCoroutine(FadeIn());
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
@@ -15,4 +22,39 @@ public class ChangeScene : MonoBehaviour
         }
     }
 
+    IEnumerator FadeIn() {
+        float t = 1f;
+
+        while (t > 0f) {
+            float a = fadeCurve.Evaluate(t);
+
+            t = t - Time.deltaTime;
+
+            //For raw images this must be done to change their alpha
+            //Per les raw images s'ha de fer això per canviar el seu alpha
+            Color color = img.color;
+            color.a = a;
+            img.color = color;
+
+            yield return 0;
+        }
+    }
+
+    IEnumerator FadeOut(string scene) {
+        float t = 0f;
+
+        while (t < 1f) {
+            float a = fadeCurve.Evaluate(t);
+
+            t = t + Time.deltaTime;
+
+            Color color = img.color;
+            color.a = a;
+            img.color = color;
+
+            yield return 0;
+        }
+
+        SceneManager.LoadScene(scene);
+    }
 }
