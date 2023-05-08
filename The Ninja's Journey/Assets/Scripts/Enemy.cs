7865190302a0810404c9871bf73ff053e67df8a6
rarboless <115485808+rarboless@users.noreply.github.com>
2023-05-08 18:19:26 +0200

    colisions enemics millorat

--- The Ninja's Journey/Assets/Scripts/Enemy.cs
+++ The Ninja's Journey/Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
 
     public string enemyName;
     public int baseAttack;
-    public float moveSpeed;
+    //public float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
