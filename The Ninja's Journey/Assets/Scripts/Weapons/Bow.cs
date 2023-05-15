79a9ae553f5c1c592aa4e329142440373a68564c
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2023-05-15 17:52:25 +0200

    pickups

--- The Ninja's Journey/Assets/Scripts/Weapons/Bow.cs
+++ The Ninja's Journey/Assets/Scripts/Weapons/Bow.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Bow : Weapons 
-{
-    public GameObject arrowPrefab;
+public class Bow : Weapons  {
+    private GameObject arrowPrefab;
 
     void Start() {
         damage = 1;
         fireForce = 20f;
         timeBetweenShots = 0.3f;
         arrowPrefab = Resources.Load<GameObject>("Assets / Prefabs / Arrow.prefab");
+
+        type = "Bow";
+
         bulletPrefab = arrowPrefab;
         weaponSprite.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Assets/Sprites/Sprite.png");
     }
--- The Ninja's Journey/Assets/Scripts/Weapons/Weapons.cs
+++ The Ninja's Journey/Assets/Scripts/Weapons/Weapons.cs
@@ -6,6 +6,7 @@ public class Weapons : MonoBehaviour {
     protected int damage;
     protected float fireForce;
     protected float timeBetweenShots;
+    protected string type;
 
     [SerializeField] protected GameObject bulletPrefab;
     [SerializeField] protected Sprite weaponSprite;
