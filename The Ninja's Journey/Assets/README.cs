6293e0382fae07443324872b244f41f93f6d7527
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2023-05-16 17:32:02 +0200

    Update README.cs

--- The Ninja's Journey/Assets/README.cs
+++ The Ninja's Journey/Assets/README.cs
@@ -1,3 +1,11 @@
 /*
-    
+    - Els checkpoints ara són visibles perque hem estat provant el seu funcionament.
+    - Quan el personatge perd vida, disminueix la seva velocitat de moviment.
+    - Hi han monedes en el mapa per recolecta i et sumaràn la puntuació.
+    - A més de les monedes com a pick-ups, també tenim picks ups curatius, fa que puguis recuperar una vida o tota depenent del tamany del cor.
+    - També hi ha pickups de les armes per poder aconseguir aquesta (només dos de implementades, de moment: arc i shuriken)
+    - Hi ha dos tipus d'enemics que hem implemenent de moment:
+        - L'enemic Skull, que et persegueix quan et veu i estiguis dins del seu radi de persecució, i et treu vida quan et toca.
+        - L'enemic Flame, que et persegueix i quan estàs en el seu radi d'atac, et comença a dispara i es queda en una distancia de tu.
+    - A més, tenim una masmorra, a la qual s'accedeix entran per la cova de l'escena principal. Allà et trobaràs enemics, curacions, monedes, altres pickups. i armes
 */
