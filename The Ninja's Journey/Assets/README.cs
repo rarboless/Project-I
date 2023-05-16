b9f057901816fed574e1f12a9bf2414ce5b4cc20
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2023-05-16 17:38:30 +0200

    Update README.cs

--- The Ninja's Journey/Assets/README.cs
+++ The Ninja's Journey/Assets/README.cs
@@ -1,11 +1,11 @@
 /*
-    - Els checkpoints ara són visibles perque hem estat provant el seu funcionament.
+    - Els checkpoints ara són visibles perquè hem estat provant el seu funcionament.
     - Quan el personatge perd vida, disminueix la seva velocitat de moviment.
-    - Hi han monedes en el mapa per recolecta i et sumaràn la puntuació.
-    - A més de les monedes com a pick-ups, també tenim picks ups curatius, fa que puguis recuperar una vida o tota depenent del tamany del cor.
-    - També hi ha pickups de les armes per poder aconseguir aquesta (només dos de implementades, de moment: arc i shuriken)
-    - Hi ha dos tipus d'enemics que hem implemenent de moment:
+    - Hi ha monedes en el mapa per recol·lecta i et sumaran la puntuació.
+    - A més de les monedes com a pick-ups, també tenim picks-ups curatius, fa que puguis recuperar una vida o tota depenent del mida del cor.
+        - També hi ha pick-ups de les armes per poder aconseguir aquesta (només dos d'implementades, de moment: arc i shuriken)
+    - Hi ha dos tipus d'enemics que hem implementant de moment:
         - L'enemic Skull, que et persegueix quan et veu i estiguis dins del seu radi de persecució, i et treu vida quan et toca.
-        - L'enemic Flame, que et persegueix i quan estàs en el seu radi d'atac, et comença a dispara i es queda en una distancia de tu.
-    - A més, tenim una masmorra, a la qual s'accedeix entran per la cova de l'escena principal. Allà et trobaràs enemics, curacions, monedes, altres pickups. i armes
+        - L'enemic Flame, que et persegueix i quan estàs en el seu radi d'atac, et comença a disparar i es queda en una distància de tu.
+    - A més, tenim una masmorra, a la qual s'accedeix entrant per la cova de l'escena principal. Allà et trobaràs enemics, curacions, monedes, altres pick-ups.
 */
