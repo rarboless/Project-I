db504790c7b80a0fa13ad8e1c1686c7ca29f70cc
rarboless <115485808+rarboless@users.noreply.github.com>
2023-05-16 17:49:53 +0200

    extra

--- The Ninja's Journey/Assets/README.cs
+++ The Ninja's Journey/Assets/README.cs
@@ -1,11 +1,11 @@
 /*
-    - Els checkpoints ara són visibles perquè hem estat provant el seu funcionament.
+    - Els checkpoints ara són visibles (fletxes blaves) perque sigui visible a la entrega, més endavant seràn invisibles i tindràn un icon per saber on són.
     - Quan el personatge perd vida, disminueix la seva velocitat de moviment.
-    - Hi ha monedes en el mapa per recol·lecta i et sumaran la puntuació.
-    - A més de les monedes com a pick-ups, també tenim picks-ups curatius, fa que puguis recuperar una vida o tota depenent del mida del cor.
-        - També hi ha pick-ups de les armes per poder aconseguir aquesta (només dos d'implementades, de moment: arc i shuriken)
-    - Hi ha dos tipus d'enemics que hem implementant de moment:
-        - L'enemic Skull, que et persegueix quan et veu i estiguis dins del seu radi de persecució, i et treu vida quan et toca.
-        - L'enemic Flame, que et persegueix i quan estàs en el seu radi d'atac, et comença a disparar i es queda en una distància de tu.
-    - A més, tenim una masmorra, a la qual s'accedeix entrant per la cova de l'escena principal. Allà et trobaràs enemics, curacions, monedes, altres pick-ups.
+    - Hi han monedes en el mapa per recol·lectar.
+    - A més de les monedes com a pick-ups, també tenim picks ups curatius, fa que puguis recuperar una vida o tota depenent de la mida del cor.
+    - També hi ha pickups de les armes per poder aconseguir aquesta (només dos d´implementades, de moment: arc i shuriken)
+    - Hi ha dos tipus d'enemics que hem implemenent de moment:
+        - L'enemic Skull, et persegueix quan estiguis dins del radi de persecució, quan no ho és torna a la seva posició inicial. Et treu vida quan et toca.
+        - L'enemic Flame, et persegueix i quan estàs en el seu radi d'atac, et comença a dispara i es queda en una distancia de tu.
+    - A més, tenim una masmorra, a la qual s'accedeix entran per la cova de l'escena principal. Allà et trobaràs enemics, curacions, monedes, altres pickups. i armes.
 */
