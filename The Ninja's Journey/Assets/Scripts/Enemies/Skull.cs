3be4b2b7c7c0bd96d933efd4989e7855503444dc
rarboless <115485808+rarboless@users.noreply.github.com>
2023-05-18 09:44:51 +0200

    minor changes

--- The Ninja's Journey/Assets/Scripts/Enemies/Skull.cs
+++ The Ninja's Journey/Assets/Scripts/Enemies/Skull.cs
@@ -20,7 +20,6 @@ public class Skull : Enemy {
 
         if (Vector2.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius) {
             transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-            //move with rb instead
             Vector2 temp = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
             rb.MovePosition(temp);
 
