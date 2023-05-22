fdaa8e076c4f45055a4cabb2fd5f478b79031987
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2023-05-22 19:39:43 +0200

    Play button function

--- The Ninja's Journey/Assets/Scripts/MainMenu.cs
+++ The Ninja's Journey/Assets/Scripts/MainMenu.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    [SerializeField] private string scene;
+    [SerializeField] private int sceneBuildIndex;
     [SerializeField] ChangeScene changeScene;
 
     // Start is called before the first frame update
@@ -17,8 +18,8 @@ public class MainMenu : MonoBehaviour {
     }
 
     public void Play() {
-        scene = "Exterior";
-        changeScene.FadeTo(scene);
+        sceneBuildIndex = 0;
+        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
     }
 
     public void Levels() {
