bf08cd6932473f12d8130093bbc506c8480263a1
rarboless <115485808+rarboless@users.noreply.github.com>
2023-05-22 18:49:08 +0200

    coin and peack

---
+++ The Ninja's Journey/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    private AudioSource audioSource;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+        else {
+            Debug.Log("More than one audio manager in the scene");
+        }
+    }
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip audio) {
+        audioSource.PlayOneShot(audio);
+    }
+}
