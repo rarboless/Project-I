34407e0e8488a19c133f6ae9819b05d126bdd112
rarboless <115485808+rarboless@users.noreply.github.com>
2023-05-23 19:35:32 +0200

    music fade

--- The Ninja's Journey/Assets/Scripts/Audio/MusicManager.cs
+++ The Ninja's Journey/Assets/Scripts/Audio/MusicManager.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour
-{
+public class MusicManager : MonoBehaviour {
     [SerializeField] private AudioSource backgroundMusic;
     [SerializeField] private AudioClip musicStart;
-    void Start()
-    {
+    void Start() {
+        StartCoroutine(FadeIn(backgroundMusic, 10));
         backgroundMusic.PlayOneShot(musicStart);
         backgroundMusic.PlayScheduled(AudioSettings.dspTime + musicStart.length);
+    }
+    public static IEnumerator FadeIn(AudioSource audioSource, float FadeTime) {
+        audioSource.Play();
+        audioSource.volume = 0f;
+        while (audioSource.volume < 1) {
+            audioSource.volume += Time.deltaTime / FadeTime;
+            yield return null;
+        }
     }
 }
