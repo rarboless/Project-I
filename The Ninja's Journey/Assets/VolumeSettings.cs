7a5189f604dd9edfcf16e8717dc461fd15b6cd76
rarboless <115485808+rarboless@users.noreply.github.com>
2023-05-30 19:39:50 +0200

    music scanes

--- The Ninja's Journey/Assets/VolumeSettings.cs
+++ The Ninja's Journey/Assets/VolumeSettings.cs
@@ -11,18 +11,34 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider sfxSlider;
 
     private void Start() {
-        SetMusicVolume();
-        SetSFXVolume();
+        if(PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("sfxVolume")) {
+            LoadVolume();
+        }
+        else {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+
     }
 
     public void SetMusicVolume() {
         float volume = musicSlider.value;
         audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
     public void SetSFXVolume() {
         float volume = sfxSlider.value;
         audioMixer.SetFloat("sfx", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    private void LoadVolume() {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SetMusicVolume();
+
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetSFXVolume();
     }
 
     
