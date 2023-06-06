ff45cfa39d027683924b98ce6e7193eeb39cf9fc
rarboless <115485808+rarboless@users.noreply.github.com>
2023-06-06 19:05:10 +0200

    new enemy Tree

---
+++ The Ninja's Journey/Assets/Scripts/Enemies/Tree.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : Enemy { 
+    void Start() {
+        target = GameObject.FindWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        if (maxHealth <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate() {
+        Movement();
+    }
+
+    void Movement() {
+        target = GameObject.FindWithTag("Player").transform;
+
+        animator.SetBool("isWalking", true);
+
+        if (Vector2.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            Vector2 temp = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            rb.MovePosition(temp);
+
+            animator.SetFloat("moveX", movement.x);
+            animator.SetFloat("moveY", movement.y);
+        }
+        else {
+            transform.position = Vector2.MoveTowards(transform.position, homePosition.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
