cd150e7c7e6e06a0609a9d1ee7d9ccd773f3ae8b
rarboless <115485808+rarboless@users.noreply.github.com>
2023-06-12 19:34:10 +0200

    Update README.cs

--- The Ninja's Journey/Assets/README.cs
+++ The Ninja's Journey/Assets/README.cs
@@ -1,20 +1,19 @@
 /*
 CAMBIOS NUEVOS:
 
-- Arma Kunai: una arma lanza 3 pryectiles juntos.
+- Arma Kunai: una arma lanza 3 proyectiles juntos.
 
 - Arreglada la partícula del shuriken.
 
 - Escena nueva con un boss. Se encuentra el camino en la zona inferior de Exterior. 
-Instancia enemigos y hace dash en forma de ataque. Al ser un boss tiene una barra. Al morir deja 10 monedas en el mapa de manera aleatoria en un circulo de radio 2 y 
-aparece un cofre, el cual al entrar en contaacto mustra las estadísticas de la partida, están manejadas por el Game Master.
-Des de este Panel, se puede volver al menú principal y se resetean las estadísticas, para poder volver a jugar.
+Instancia enemigos y hace dash en forma de ataque. Al ser un boss tiene una barra. Al morir deja 10 monedas en el mapa de manera aleatoria en un círculo de radio 2 y 
+aparece un cofre, el cual al entrar en contacto muestra las estadísticas de la partida, están manejadas por el Game Master.
+Desde este Panel, se puede volver al menú principal y se resetean las estadísticas, para poder volver a jugar.
 
 - Escena nueva con una nueva planta de la dungeon.
 
-- Hay un nuevo enemigo, bamboo. Va a gran velocidad cúando entras en su rango i se tiene que reaccionar rapiso para matarlo ya que va a gran velocidad.
-Cúando te toca quita vida y se destruye.
+- Hay un nuevo enemigo, bamboo. Va a gran velocidad cuándo entras en su rango y se tiene que reaccionar rápido para matarlo, ya que va a gran velocidad.
+Cuándo te toca, quita vida y se destruye.
 
-- Matar a un eneigo añade una moneda y ahora tiene animaciones extra, cambia según la direccón en la que mire.
-
+- Matar a un enemigo añade una moneda y ahora tiene animaciones extra, cambia según la dirección en la que mire.
 */
