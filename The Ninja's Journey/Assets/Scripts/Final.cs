2d7f1ac85699690e1ec5254c957198e24ebaa1cb
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2023-06-13 09:50:53 +0200

    pause menu bug fixed

--- The Ninja's Journey/Assets/Scripts/Final.cs
+++ The Ninja's Journey/Assets/Scripts/Final.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Final : MonoBehaviour
-{
+public class Final : MonoBehaviour {
     [SerializeField] private GameObject finalPanel;
+
+    private PauseMenu pmScript;
 
     void Start() {
         finalPanel.SetActive(false);
+        pmScript = GameObject.FindGameObjectWithTag("Pause").GetComponent<PauseMenu>();
+        pmScript.enabled = true;
     }
 
     void Update() {
@@ -29,10 +32,9 @@ public class Final : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.CompareTag(tag: "Player")) {
+        if (collision.CompareTag(tag: "Player")) {
             Toggle();
+            pmScript.enabled = false;
         }
     }
-
-
 }
