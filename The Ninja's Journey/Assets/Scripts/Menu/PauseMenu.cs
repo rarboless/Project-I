4ec71a8d60cfe9e4c8a344fd5bd17f400a9e8792
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2023-06-13 19:38:40 +0200

    Revert "pause menu animation"
    This reverts commit f2d54b44542f82475f8963ef7f5eed075e6c1953.

--- The Ninja's Journey/Assets/Scripts/Menu/PauseMenu.cs
+++ The Ninja's Journey/Assets/Scripts/Menu/PauseMenu.cs
@@ -2,26 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
     [SerializeField] private GameObject pausePanel;
 
-    [SerializeField] private Animator pauseMenuAnimator;
-    private bool isOpen;
-
     // Start is called before the first frame update
     void Start() {
         pausePanel.SetActive(false);
-        isOpen = false;
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            isOpen = !isOpen;
-            Debug.Log(isOpen);
-            pauseMenuAnimator.SetBool("IsOpen", isOpen);
             Toggle();
         }
     }
